4910c2a49ef2e801a6e01648047f8eeecbb607b2
sehyunyuk <sdfg8931@ajou.ac.kr>
2024-11-27 02:23:09 +0900

    add side bar

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,27 @@ const buttons = document.querySelectorAll("button");
 for (const button of buttons) {
   button.addEventListener("click", createParagraph);
 }
+
+
+
+document.addEventListener('mousemove', function(e) {
+  const container = document.querySelector('.container');
+// && !container.classList.contains('sidebar-active')
+  // 마우스가 화면 왼쪽 20px 이내로 들어오면 사이드바 활성화
+  if (e.clientX < 20  && !container.classList.contains('sidebar-active')) {
+    container.classList.add('sidebar-active');
+    console.log(container.classList)
+  }
+});
+
+
+document.addEventListener('mousemove', function(e) {
+  const container = document.querySelector('.container');
+  const sidebarContainer = document.querySelector('.sidebar-container');
+
+  if (container.classList.contains('sidebar-active') &&
+          e.clientX > 400 ) {
+    container.classList.remove('sidebar-active');
+  }
+});
+
